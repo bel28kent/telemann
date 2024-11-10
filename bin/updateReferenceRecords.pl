01125ca30c45853858d509dfe8ce77c2b40917f5
Bryan Bell <bjbsga@gmail.com>
2024-11-09 21:39:55 -0800

    add reference record tags to script

--- bin/updateReferenceRecords.pl
+++ bin/updateReferenceRecords.pl
@@ -9,3 +9,47 @@
 
 use strict;
 use warnings;
+
+__DATA__
+key
+COM
+CDT
+CBL
+CDL
+CNT
+TWV
+TWV-genre
+TWV-work
+OPR
+OTL
+OMD
+OMV
+ODT
+AST
+AGN
+AMT
+AIN
+RTL1
+RPN1
+RMM1
+RC#1
+RRD1
+RLC1
+RDT1
+RT#1
+RTL2
+RPN2
+RMM2
+RC#2
+RRD2
+RLC2
+RDT2
+RT#2
+RTL3
+RPN3
+RMM3
+RC#3
+RRD3
+RLC3
+RDT3
+RT#3
