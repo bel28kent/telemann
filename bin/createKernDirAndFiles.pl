b74a5264c204bad6787de6e0d450be4219267cb9
Bryan Bell <bjbsga@gmail.com>
2024-11-10 13:55:03 -0800

    add createKernDirAndFiles executable

---
+++ bin/createKernDirAndFiles.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+# Programmer:  Bryan Jacob Bell
+# Begun:       Sun Nov 10 13:47:24 PST 2024
+# Modified:    Sun Nov 10 13:47:24 PST 2024
+# File:        createKernDirAndFiles.pl
+# Syntax:      Perl 5
+# Description: make a directory with kern files
+#
+# files are initialized with reference records from
+# metadata/reference_records.tsv
+
+use strict;
+use warnings;
