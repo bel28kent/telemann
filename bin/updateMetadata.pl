a02012c1ed261fc15c18db9e3dd25dd7afa7910a
Bryan Bell <bjbsga@gmail.com>
2024-11-09 20:46:10 -0800

    add bin dir

---
+++ bin/updateMetadata.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+# Programmer:  Bryan Jacob Bell
+# Begun:       Sat Nov  9 20:33:36 PST 2024
+# Modified:    Sat Nov  9 20:33:36 PST 2024
+# File:        updateMetadata.pl
+# Syntax:      Perl 5
+# Description: get data from telemann_metadata spreadsheets
+
+use strict;
+use warnings;
---
+++ bin/updateReferenceRecords.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+# Programmer:  Bryan Jacob Bell
+# Begun:       Sat Nov  9 20:42:53 PST 2024
+# Modified:    Sat Nov  9 20:42:53 PST 2024
+# File:        updateReferenceRecords.pl
+# Syntax:      Perl 5
+# Description: update reference records in kern files
+
+use strict;
+use warnings;
