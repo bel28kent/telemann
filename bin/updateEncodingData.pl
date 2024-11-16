fcf51eccc95531325573bc726efe4280c304d5b7
Bryan Bell <bjbsga@gmail.com>
2024-11-15 22:38:56 -0800

    add updateEncodingData script

---
+++ bin/updateEncodingData.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+# Programmer:  Bryan Jacob Bell
+# Begun:       Fri Nov 15 22:36:14 PST 2024
+# Modified:    Fri Nov 15 22:36:14 PST 2024
+# File:        updateEncodingData.pl
+# Syntax:      Perl 5
+# Description: updates telemann/metadata/encoding_data.tsv
+
+use strict;
+use warnings;
