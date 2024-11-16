58b149d350861197fce88b1f19804b6932918a41
Bryan Bell <bjbsga@gmail.com>
2024-11-16 11:55:40 -0800

    add reportEncodingStatus script

---
+++ bin/reportEncodingStatus.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+# Programmer:  Bryan Jacob Bell
+# Begun:       Sat Nov 16 11:46:46 PST 2024
+# Modified:    Sat Nov 16 11:46:46 PST 2024
+# File:        reportEncodingStatus.pl
+# Syntax:      Perl 5
+# Description: report status of encoding process
+#
+#    -e, only report works that have been encoded
+#    -E, report encoding status of all works
+#    -v, only report works that have been verified in vhv
+#    -V, report vhv checking of all works
+#    -ev or -EV, report both
+#    -eV or Ev, error
+
+use strict;
+use warnings;
